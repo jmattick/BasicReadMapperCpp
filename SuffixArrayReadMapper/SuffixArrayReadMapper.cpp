052af41e2e1076cf7d3308f50155c9bfbad63c07
Jessica Mattick <jmatty1661@gmail.com>
2021-09-03 01:42:35 -0500

    first commit

---
+++ SuffixArrayReadMapper/SuffixArrayReadMapper.cpp
@@ -0,0 +1,12 @@
+﻿// SuffixArrayReadMapper.cpp : Defines the entry point for the application.
+//
+
+#include "SuffixArrayReadMapper.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello From Suffix Array Read Mapper!" << endl;
+	return 0;
+}
