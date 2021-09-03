7d2b264d830044e26efcd0ed91e64c74fdf71ea4
jmattick <jmattick16@gmail.com>
2021-09-03 17:15:17 +0000

    first cmake

---
+++ SuffixArrayReadMapper/CMakeFiles/SuffixArrayReadMapper.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SuffixArrayReadMapper.dir/SuffixArrayReadMapper.cpp.o"
+  "SuffixArrayReadMapper.pdb"
+  "SuffixArrayReadMapper"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SuffixArrayReadMapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
