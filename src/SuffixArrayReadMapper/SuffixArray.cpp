0ffc280034453ae479b9772574036fc27674952d
Jessica Mattick <jmatty1661@gmail.com>
2021-09-05 14:04:49 -0500

    fix suffix array sorting

--- src/SuffixArrayReadMapper/SuffixArray.cpp
+++ src/SuffixArrayReadMapper/SuffixArray.cpp
@@ -33,21 +33,24 @@ std::size_t * GenerateSuffixArray(std::string seq, std::size_t n)
 	//sort the suffixes according to the first two characters rank
 	std::sort(suffixes, suffixes + n, compareSuffix);
 
-	std::size_t* ind = new std::size_t[n]; //array to get ind in suffixes from original index
-
+	//array to get ind in suffixes from original index
+	std::size_t* ind = new std::size_t[n]; 
+	
+	
 	for (std::size_t i = 4; i < 2 * n; i = i * 2)
 	{
 		//assign rank and index for first suffix
 		int rank = 0;
 		int prev_rank = suffixes[0].rank[0];
-		suffixes[0].rank[0] = 0;
+		suffixes[0].rank[0] = rank;
 		ind[suffixes[0].index] = 0;
 
 		//assign rank to remaining suffixes
 		for (std::size_t j = 1; j < n; j++)
 		{
 			//if first rank and next rank are the same as previous suffix in array
-			if (suffixes[j].rank[0] == prev_rank && suffixes[j].rank[1] == prev_rank)
+			if (suffixes[j].rank[0] == prev_rank && 
+				suffixes[j].rank[1] == prev_rank)
 			{
 				//assign the same new rank to suffix
 				prev_rank = suffixes[j].rank[0];
@@ -64,8 +67,7 @@ std::size_t * GenerateSuffixArray(std::string seq, std::size_t n)
 		//asign next rank to suffixes
 		for (std::size_t k = 0; k < n; k++)
 		{
-			std::size_t modifier = (k % 2 == 0) ? k / 2 : (k - 1) / 2;
-			std::size_t nextInd = suffixes[k].index + modifier;
+			std::size_t nextInd = suffixes[k].index + i/2;
 			suffixes[k].rank[1] = (nextInd < n)? suffixes[ind[nextInd]].rank[0]: -1;
 		}
 		
